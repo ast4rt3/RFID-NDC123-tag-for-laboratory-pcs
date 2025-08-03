92d7c36f8b1b1158d178186dbf0c8dba2a6d2e48
ast4rt3 <lawrenze3206@gmail.com>
2025-08-03 19:43:38 +0800

    Building the dedicated mobile app scanner

---
+++ mobile-app/RFIDMobileApp/app/(tabs)/rfid.tsx
@@ -0,0 +1,5 @@
+// filepath: mobile-app/RFIDMobileApp/app/(tabs)/rfid.tsx
+import { ThemedText } from '@/components/ThemedText';
+export default function RFIDScreen() {
+  return <ThemedText>RFID Screen</ThemedText>;
+}
