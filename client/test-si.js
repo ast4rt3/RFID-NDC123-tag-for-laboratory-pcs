2777427fd22b26a97c7635d152ff38d4437176fe
ast4rt3 <lawrenze3206@gmail.com>
2025-11-28 01:16:12 +0800

    cleanup

--- client/test-si.js
+++
@@ -1,7 +0,0 @@
-const si = require('systeminformation');
-
-si.users()
-    .then(data => {
-        console.log('Users data:', JSON.stringify(data, null, 2));
-    })
-    .catch(error => console.error(error));
